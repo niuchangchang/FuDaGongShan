9f6d6c8b58f0736920a4baaf125f46a4b877ac5c
niuchang12@126.com <niuchang12@126.com>
2023-08-04 13:59:33 +0800

    feat: update vuex

--- store/index.js
+++ store/index.js
@@ -3,23 +3,64 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+const ACCESSTOKEN = uni.getStorageSync('accessToken') || '';
+const USERINFO = uni.getStorageSync('userInfo') || {};
+const CARTNUM = uni.getStorageSync('cartNum') || 0;
+
+
 const store = new Vuex.Store({
 	state: {
+		// 用户token
+		accessToken: ACCESSTOKEN,
+		// 用户信息
+		userInfo: USERINFO,
 		// 网络状态
 		networkState: 'unknown',
+		// 购物车数字角标
+		cartNum: CARTNUM,
 	},
 	getters: {
-		// isLogin: state => Object.keys(state.member).length > 0	//是否登录
+		// 获取网络状态
+		networkStatus: state => {
+			return state.networkState;
+		},
+		// 判断用户是否登录
+		hasLogin: state => {
+			return !!state.accessToken;
+		},
 	},
 	mutations: {
+		login(state, provider) {
+			state.accessToken = provider;
+			uni.setStorageSync('accessToken', provider);
+			// state.userInfo = provider.userInfo;
+			// uni.setStorageSync('userInfo', provider.userInfo);
+		},
+		logout(state) {
+			state.accessToken = '';
+			state.userInfo = {};
+			uni.removeStorageSync('accessToken');
+			uni.removeTabBarBadge({ index: $mConstDataConfig.cartIndex });
+			uni.removeStorageSync('user');
+		},
+		setUserInfo(state, userInfo) {
+			uni.setStorageSync('userInfo', userInfo);
+		},
 		setNetworkState(state, provider) {
 			state.networkState = provider;
+		},
+		setCartNum(state, provider) {
+			state.cartNum = provider;
+			uni.setStorageSync('cartNum', provider);
 		},
 	},
 	actions: {
 		networkStateChange({ commit }, info) {
 			commit('setNetworkState', info);
 		},
+		logout({ commit }) {
+			commit('logout');
+		}
 	}
 })
 
