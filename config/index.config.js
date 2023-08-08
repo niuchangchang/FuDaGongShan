2666e62d5f2f08a78fea9fe874a5def397a71948
niuchang12@126.com <niuchang12@126.com>
2023-08-08 14:17:36 +0800

    update api

--- config/index.config.js
+++ config/index.config.js
@@ -2,13 +2,12 @@ const CONFIG = {
     // 开发环境配置
     development: {
         assetsPath: '/static', // 静态资源路径
-		// baseUrl: '', // 后台接口请求地址
-		baseUrl: 'https://wearable.zrsy-mpi.com:8013/backend/app', // 后台接口请求地址
+		baseUrl: 'https://jy.e314.cn', // 后台接口请求地址
     },
     // 生产环境配置
     production: {
         assetsPath: '/static', // 静态资源路径
-        baseUrl: '', // 后台接口请求地址
+        baseUrl: 'https://jy.e314.cn', // 后台接口请求地址
     }
 
 };
